0c77e4e7ce8846e76e9af45056018348eaaf1056
Umidzhon Karabaev <umidzhonfullstackdev@gmail.com>
2023-07-19 00:16:32 +0600

    Challenge-5

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
+import React from "react";
 import "./App.css";
-import Card from "./components/Card";
-import movies from "./movies";
 
-function App() {
-  return (
-    <div className="container">
-      <h1 className="heading">My movies</h1>
-      <div className="cards">
-        {movies.map((movie, index) => {
-          return <Card key={index} props={movie} />;
-        })}
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  addDeleteReset = (value) => {
+    this.setState({ count: value });
+  };
+
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <div className="card">
+                <div className="card-header">
+                  <h1>Counter: {this.state.count}</h1>
+                </div>
+                <div className="card-body">
+                  <button
+                    className="btn btn-success"
+                    onClick={() => this.addDeleteReset(this.state.count + 1)}
+                  >
+                    Increment
+                  </button>
+                  <button
+                    className="btn btn-primary m-2"
+                    onClick={()=> this.addDeleteReset(0)}
+                  >
+                    Reset
+                  </button>
+                  <button
+                    className="btn btn-danger m-2"
+                    onClick={() => this.addDeleteReset(this.state.count - 1)}
+                  >
+                    Decrement
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
