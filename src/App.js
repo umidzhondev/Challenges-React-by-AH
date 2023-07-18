11222b36b0da5f2ae2fe52aa55287eb46b4c0f1c
Umidzhon Karabaev <umidzhonfullstackdev@gmail.com>
2023-07-19 00:12:50 +0600

    Challenge-4: Added three methods

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
+import React from "react";
 import "./App.css";
-import Card from "./components/Card";
-import movies from "./movies";
 
-function App() {
-  return (
-    <div className="container">
-      <h1 className="heading">My movies</h1>
-      <div className="cards">
-        {movies.map((movie, index) => {
-          return <Card key={index} props={movie} />;
-        })}
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  addOne = ()=>{
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
+  deleteOne = ()=>{
+    this.setState({
+      count: this.state.count - 1,
+    })
+  }
+  reset = ()=>{
+    this.setState({
+      count: 0,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <div className="card">
+                <div className="card-header">
+                  <h1>Counter: {this.state.count}</h1>
+                </div>
+                <div className="card-body">
+                  <button className="btn btn-success" onClick={this.addOne}>
+                    Increment
+                  </button>
+                  <button className="btn btn-primary m-2" onClick={this.reset}>
+                    Reset
+                  </button>
+                  <button
+                    className="btn btn-danger m-2"
+                    onClick={this.deleteOne}
+                  >
+                    Decrement
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
