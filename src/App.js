73cbfb5144b8d7916b8849d6698fb88a0ccdc5ac
Umidzhon Karabaev <umidzhonfullstackdev@gmail.com>
2023-07-27 23:59:50 +0600

    Challenge-9: Email Checked

--- src/App.js
+++ src/App.js
@@ -1,148 +1,83 @@
 import React, { Component } from "react";
-import User from "./components/User";
 
 class App extends Component {
   state = {
-    users: [],
-    name: "",
-    model: "",
-    number: "",
+    email: "",
+    isAgree: false,
   };
 
-  componentDidMount() {
-    const users = [
-      {
-        name: "Karabaev.U",
-        model: "Lexux",
-        number: "0657",
-      },
-      {
-        name: "Fayzullaev.N",
-        model: "Tayota",
-        number: "4519",
-      },
-      {
-        name: "Rejabaliev.U",
-        model: "BMW",
-        number: "0205",
-      },
-    ];
-    this.setState({ users: users });
-  }
-
-  deleteUser = (index) => {
-    let newUsers = this.state.users;
-    newUsers.splice(index, 1);
-    this.setState({ users: newUsers });
-  };
-
-  addUser = () => {
-    const {name ,model,number,users} = this.state;
-    let newUser = {
-      name,
-      model,
-      number
-    }
-    users.push(newUser);
+  handleEmail = (e) => {
     this.setState({
-      users,
-      name: "",
-      model: "",
-      number: "",
-    })
-
+      email: e.target.value,
+    });
   };
 
-  handleInput = (e) => {
-    this.setState({ 
-      [e.target.name]: e.target.value
+  handleIsAgree = (e) => {
+    this.setState({
+      isAgree: e.target.checked,
     });
-  }
+  };
 
+  submitBtn = () => {
+    const regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    ;
+    const isValidEmail = regEmail.test(this.state.email)
+    const isValidIsAgree = this.state.isAgree;
 
+    if(!isValidEmail){
+      alert("Please enter a valid email")
+      return
+    }
+    if(!isValidIsAgree){
+      alert("Please, agree with terms")
+      return
+    }
+
+    this.setState({
+      email: "",
+      isAgree:""
+    })
+    alert("Congratulations")
+  };
 
   render() {
-    const { users } = this.state;
+    const { email, isAgree } = this.state;
     return (
-      <div className="container">
-        <div className="radar m-4">
-          <h1 className="text-center mb-5">Radar </h1>
-
-          <h2 className="text-left mb-4">Yangi Qo'shish:</h2>
-          <div className="row radar__header d-flex align-items-end">
-            <div className="col-3">
-              <label className="mb-2 fs-5" htmlFor="name">
-                Ism va familiyasi:
-              </label>
-              <input
-                value={this.state.name}
-                onChange={this.handleInput}
-                type="text"
-                className="form-control"
-                name="name"
-                id="name"
-              />
-            </div>
-            <div className="col-3">
-              <label className="mb-2 fs-5" htmlFor="model">
-                Modeli:
-              </label>
-              <input
-                value={this.state.model}
-                onChange={this.handleInput}
-                type="text"
-                className="form-control"
-                name="model"
-                id="model"
-              />
-            </div>
-            <div className="col-3">
-              <label className="mb-2 fs-5" htmlFor="number">
-                Raqami:
-              </label>
-              <input
-                value={this.state.number}
-                onChange={this.handleInput}
-                type="text"
-                className="form-control"
-                name="number"
-                id="number"
-              />
-            </div>
-            <div className="col-3">
-              <button onClick={this.addUser} className="btn btn-primary">
-                Qo'shish
-              </button>
-            </div>
+      <div className="contact">
+        <div className="contact-card">
+          <div className="contact-title">
+            <h2>Contact Us</h2>
           </div>
-
-          <div className="row radar__body">
-            <table className="table table-hover table-striped mt-5">
-              <thead className="table-dark">
-                <tr>
-                  <th>№</th>
-                  <th>Ism va familiyasi</th>
-                  <th>Modeli</th>
-                  <th>Raqami</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.map((user, index) => {
-                  const { name, model, number } = user;
-                  return (
-                    <User
-                      key={index}
-                      listNumber={index}
-                      name={name}
-                      model={model}
-                      number={number}
-                      deleteUser={this.deleteUser}
-                    />
-                  );
-                })}
-              </tbody>
-            </table>
+          <div className="contact-form">
+            <form action="#">
+              <div>
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <input
+                  className="form-control mb-3"
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={email}
+                  onChange={this.handleEmail}
+                />
+              </div>
+              <div>
+                <input
+                  className="form-checkbox-input my-3 mx-2"
+                  type="checkbox"
+                  name="agreement"
+                  id="agreement"
+                  checked={isAgree}
+                  onChange={this.handleIsAgree}
+                />
+                <label htmlFor="agreement">Terms & Conditions</label>
+              </div>
+              <button onClick={this.submitBtn} className="btn btn-success text-end ml-auto mt-2">
+                Submit
+              </button>
+            </form>
           </div>
         </div>
       </div>
